990e8de2a1f5799640cf603392bd4c1124a1e212
melsaka <elsmo105@student.otago.ac.nz>
2022-08-21 18:15:25 +1200

    Upload New File

---
+++ ProjectFiles/test-db.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Assets (
+  code varchar(7),
+  name varchar(50) NOT NULL,
+  PRIMARY KEY (user)
+);
+
+INSERT INTO papers VALUES ('Scott Cameron','123');
+INSERT INTO papers VALUES ('Martin Velez','456');
